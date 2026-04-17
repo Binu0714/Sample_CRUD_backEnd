a2718e3735c510d6153538263bd53896e91d7746
Binu0714 <binuj714@gmail.com>
2026-04-17 15:09:40 +0530

    feat(auth): add login logic to authController

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -28,5 +28,33 @@ export const signUp = async (req: Request, res: Response) => {
         console.error('Error during sign up:', error);
         return res.status(500).json({ error: 'Internal server error' });
     }
+}
 
+export const LogIn = async (req: Request, res: Response) => {
+    try{
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ error: 'Email and password are required' });
+        }
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        if (error) {
+            console.log("Login Error:", error.message);
+            return res.status(401).json({ error: error.message });
+        }
+
+        return res.status(200).json({
+            message: 'Login successful',
+            user: data.user,
+            session: data.session,
+        });
+
+    } catch (err) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
